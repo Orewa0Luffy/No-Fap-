98aee4b4f5d4c8f303e99f40b90ad3b5a029acd9
Orewa0Luffy <ichigobotam1@gmail.com>
2025-07-17 13:07:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function startApp() {
-  alert("Let's Go! Zoro never gives up 💪");
+  alert("Let's Go! Zoro never gives up 💪🔥");
+  // Here you can redirect to the next screen (day plan, streak, etc.)
+  // Example: window.location.href = "dailyplan.html";
 }
